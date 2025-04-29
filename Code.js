70e8907f0b0d3b1af1ed331c91b567aead5f1875
Tural Novruzov <tural.nt.1@gmail.com>
2025-04-29 14:36:50 -0400

    Add getAuthType function to return no authentication required for the connector

---
+++ Code.js
@@ -0,0 +1,10 @@
+/**
+ * Returns the authentication method required by the connector.
+ * @return {Object} AuthType configuration indicating no auth is required
+ */
+function getAuthType() {
+  var cc = DataStudioApp.createCommunityConnector();
+  return cc.newAuthTypeResponse()
+    .setAuthType(cc.AuthType.NONE)
+    .build();
+}
